73facaae8245d53b2f11e0b69833c6beec1e5056
leowilson <leobca2018@gmail.com>
2022-08-21 09:26:19 +0530

    add-script-file.

---
+++ script.js
@@ -0,0 +1,94 @@
+// date fuunction 
+var date = new Date();
+console.log(date);
+
+var previousDate = new Date(2000,10,17,9,15,22);
+var store = previousDate.setTime(12);
+var minutes =previousDate.setMinutes(40);
+
+
+// get year
+// console.log (new Date().getFullYear());
+// // get month
+// console.log (new Date().getMonth());
+// // get day 
+// console.log(new Date().getDay());
+// // get date
+// console.log(new Date().getDate());
+// // get time
+// console.log(new Date().getTime());
+// // get hour
+// console.log(new Date().getHours());
+// // get minutes
+// console.log(new Date().getMinutes());
+// // get seconds
+// console.log(new Date().getSeconds());
+// // get milliseconds
+// console.log(new Date().getMilliseconds());
+// // get timezone offset
+// console.log(new Date().getTimezoneOffset());
+
+// // UTC time
+// console.log('UTC-Date : ',new Date().getUTCDate());
+// console.log('UTC-Day : ',new Date().getUTCDay());
+// console.log('UTC-Month : ',new Date().getUTCMonth());
+// console.log('UTC-FullYear : ',new Date().getUTCFullYear());
+// console.log('UTC-Hours : ',new Date().getUTCHours());
+// console.log('UTC-Minutes : ',new Date().getUTCMinutes());
+// console.log('UTC-Seconds : ',new Date().getUTCSeconds());
+// console.log('UTC-Milliseconds : ',new Date().getUTCMilliseconds());
+
+// // set time function
+// var set = new Date('August 19, 1975 23:15:30');
+// set.setDate(25);
+// console.log(set.getDate());
+// // set month
+// var month = new Date(2000,8,15);
+// month.setMonth(3);
+// console.log(month.getMonth());
+
+// // math function
+// console.log('Floor :',Math.floor(2.8));
+// console.log("Round :",Math.round(3.5));
+// console.log("Ceil", Math.ceil(5.2));
+// console.log("Random :", Math.random());
+// console.log("Sqrt :" ,Math.sqrt(81));
+// console.log('Trunc :',Math.trunc(25.567));
+// console.log("Absolute :" , Math.abs((2)*(-5)));
+// console.log("Cube :",Math.cbrt(125));
+// console.log('Pi :',Math.PI);
+// console.log("Min :",Math.min(3,5,-8));
+// console.log('Max :',Math.max(4,9,-2));
+// console.log('Sqrt_2 :',Math.SQRT1_2);
+// console.log('Sqrt_2 :',Math.SQRT2);
+// console.log('Log2 :',Math.log2);
+// console.log('Log10 :',Math.log10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
